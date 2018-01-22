bad60c1ed2d6c49b0562e868d3d91907baa29764
Hao He <hehao1998@pku.edu.cn>
2018-01-22 21:14:04 +0800

    Add GLFW library

---
+++ glfwtest.cpp
@@ -0,0 +1,23 @@
+//
+// A simple test to check whether the GLFW library is working.
+//
+// Created by hehao on 18-1-22.
+//
+
+#include <iostream>
+
+// GLAD: A library that wraps OpenGL functions to make things easier
+//       Note that GLAD MUST be included before GLFW
+#include "glad/glad.h"
+// GLFW: A library that helps us manage windows
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+}
