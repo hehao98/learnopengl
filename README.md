232775da0af236e1434e237f9ea276c2f041e6c9
Hao He <hehao1998@pku.edu.cn>
2018-01-23 09:39:40 +0800

    Add glfwtest

--- README.md
+++ README.md
@@ -4,14 +4,4 @@ This is a repository that contains experimental and exercise
 code during my learning process of OpenGL. I'm tying to make some of
 them interesting if I have enough graphic knowledge and skills.
 #### Compile
-Currently the CMakeLists.txt only support Ubuntu system. It is likely
-to work on other Linux based systems but not for OS X and Windows.
-The CMakeLists.txt file need to be modified, before using it on other
-systems. (Especially the part to link external libraries like GLFW,
-GLAD and GLM)
-
-To install OpenGL on Ubuntu, simply type:
-
-`$ sudo apt-get install build-essential`
-
-`$ sudo apt-get install libgl1-mesa-dev`
+Currently it works only on Unix-alike systems.
--- glfwtest.cpp
+++ glfwtest.cpp
@@ -12,12 +12,65 @@
 // GLFW: A library that helps us manage windows
 #include <GLFW/glfw3.h>
 
+// Sometimes user might resize the window. so the OpenGL viewport should be adjusted as well.
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height);
+// User input is handled in this function
+void processInput(GLFWwindow *window);
 
 int main()
 {
+    // Initialization of GLFW context
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Create a window object
+    GLFWwindow *window = glfwCreateWindow(800, 600, "glfwtest", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD before calling OpenGL functions
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Tell OpenGL the size of rendering window
+    glViewport(0, 0, 800, 600);
+
+    // Set the windows resize callback function
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        // Handle user input
+        processInput(window);
+
+        // All the rendering is done here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
