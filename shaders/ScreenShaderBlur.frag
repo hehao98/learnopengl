36ca8f43ce8fa3a034685971e657c904b7b948ff
Hao He <hehao1998@pku.edu.cn>
2018-02-07 10:10:21 +0800

    post processing effect

---
+++ shaders/ScreenShaderBlur.frag
@@ -0,0 +1,36 @@
+#version 330 core
+
+out vec4 fragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+void main() {
+	float offset = 1.0/300.0;
+	vec2 offsets[] = vec2[](
+	    vec2(-offset, offset),
+	    vec2(0, offset),
+	    vec2(offset, offset),
+	    vec2(-offset, 0),
+	    vec2(0, 0),
+	    vec2(offset, 0),
+	    vec2(-offset, -offset),
+	    vec2(0, -offset),
+	    vec2(offset, -offset)
+	);
+	float kernel[9] = float[](
+	    1, 2, 1,
+	    2, 4, 2,
+	    1, 2, 1
+	);
+	for (int i = 0; i < 9; ++i)
+	    kernel[i] /= 16;
+	vec3 sampleTex[9];
+	for (int i = 0; i < 9; ++i)
+	    sampleTex[i] = vec3(texture(screenTexture, TexCoords + offsets[i]));
+	vec3 color = vec3(0.0);
+	for (int i = 0; i < 9; ++i)
+	    color += kernel[i] * sampleTex[i];
+    fragColor = vec4(color, 1.0);
+}
---
+++ src/AdvancedOpenGL/PostProcessing-Blur.cpp
@@ -0,0 +1,467 @@
+//
+// Created by hehao on 18-2-7.
+//
+
+#include <iostream>
+#include <algorithm>
+
+// GLM Math Library
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// GLAD: A library that wraps OpenGL functions to make things easier
+//       Note that GLAD MUST be included before GLFW
+#include "glad/glad.h"
+// GLFW: A library that helps us manage windows
+#include <GLFW/glfw3.h>
+
+// Wrapper classes to make things a little easier
+#include "Shader.h"
+#include "Camera.h"
+#include "Texture.h"
+
+int gScreenWidth = 800;
+int gScreenHeight = 600;
+
+float gDeltaTime = 0.0f;
+float gLastFrame = 0.0f;
+
+Camera gCamera;
+
+// Perform necessary initialization.
+// Returns pointer to a initialized window with OpenGL context set up
+GLFWwindow *init();
+// Sometimes user might resize the window. so the OpenGL viewport should be adjusted as well.
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height);
+// User input is handled in this function
+void processInput(GLFWwindow *window);
+// Mouse input is handled in this function
+void mouseCallback(GLFWwindow *window, double xpos, double ypos);
+void scrollCallback(GLFWwindow *window, double offsetX, double offsetY);
+
+int main()
+{
+    GLFWwindow *window = init();
+    if (window == nullptr) {
+        std::cout << "Failed to initialize GLFW and OpenGL!" << std::endl;
+        return -1;
+    }
+
+    Texture groundTexture("textures/ground.jpg");
+    Texture ambientMap("textures/container2.png");
+    Texture specularMap("textures/container2_specular.png");
+    Texture grassTexture("textures/grass.png");
+    Texture transparentWindowTexture("textures/blending_transparent_window.png");
+
+    // Load shaders
+    Shader objectShader("shaders/MultipleLights.vert", "shaders/Discard.frag");
+    objectShader.use();
+    objectShader.setInt("material.diffuse", 0);
+    objectShader.setInt("material.specular", 1);
+    objectShader.setInt("material.emission", 2);
+
+    Shader transparentWindowShader("shaders/MultipleLights.vert", "shaders/BasicFrag.frag");
+    transparentWindowShader.use();
+    transparentWindowShader.setInt("material.diffuse", 0);
+    transparentWindowShader.setInt("material.specular", 1);
+    transparentWindowShader.setInt("material.emission", 2);
+
+    Shader screenShader("shaders/ScreenShaderDefault.vert", "shaders/ScreenShaderBlur.frag");
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    // A set of cubeVertices to describe a cube(with normal vectors)
+    float cubeVertices[] = {
+            // positions          // normals           // texture coords
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+    };
+
+    // Vertex Buffer Objects(VBO) are used to pass cubeVertices to GPU for the vertex objectShader
+    unsigned int cubeVBO;
+    // 1 is assigned as the unique ID to this cubeVBO
+    glGenBuffers(1, &cubeVBO);
+
+    // Vertex Array Objects(VAO) are used to store vertex attribute pointers
+    unsigned int cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    // Tell OpenGL how tp interpret vertex data
+    // Pass data to layout(location=0), each data 3 values, type float, no normalization,
+    // with the stride as 6*sizeof(float)
+    // location 0: vertex location
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Vertices data to describe a ground
+    float groundVertices[] = {
+            // Positions    // Normals        // Texture coordinates
+            -0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+            0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 10.0f, 0.0f,
+            -0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 10.0f,
+            -0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 10.0f,
+            0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 10.0f, 0.0f,
+            0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 10.0f, 10.0f,
+    };
+    unsigned int groundVBO;
+    glGenBuffers(1, &groundVBO);
+    unsigned int groundVAO;
+    glGenVertexArrays(1, &groundVAO);
+    glBindVertexArray(groundVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, groundVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(groundVertices), groundVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // texture coorinate to describe a plane
+    float planeVertices[] = {
+            // Positions    // Normals        // Texture coordinates
+            -0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+            0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            -0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+            -0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+    };
+    unsigned int planeVBO;
+    glGenBuffers(1, &planeVBO);
+    unsigned int planeVAO;
+    glGenVertexArrays(1, &planeVAO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+    float quadVertices[] = {
+            // positions   // texCoords
+            -1.0f,  1.0f,  0.0f, 1.0f,
+            -1.0f, -1.0f,  0.0f, 0.0f,
+            1.0f, -1.0f,  1.0f, 0.0f,
+
+            -1.0f,  1.0f,  0.0f, 1.0f,
+            1.0f, -1.0f,  1.0f, 0.0f,
+            1.0f,  1.0f,  1.0f, 1.0f
+    };
+    unsigned int quadVBO;
+    glGenBuffers(1, &quadVBO);
+    unsigned int quadVAO;
+    glGenVertexArrays(1, &quadVAO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Initialize a customized frame buffer
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    // Generate texture;
+    unsigned int texColorBuffer;
+    glGenTextures(1, &texColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, gScreenWidth, gScreenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // Attach it to currently bind framebuffer
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+
+    // Create a render buffer object to store depth and stencil buffer
+    unsigned int RBO;
+    glGenRenderbuffers(1, &RBO);
+    glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, gScreenWidth, gScreenHeight);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    // Bind the render buffer object to framebuffer
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        std::cout << "Error: Incomplete framebuffer!" << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    gCamera.Position = glm::vec3(0.0f, 1.5f, 3.0f);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window)) {
+        // Calculate how much time since last frame
+        auto currentFrame = (float)glfwGetTime();
+        gDeltaTime = currentFrame - gLastFrame;
+        gLastFrame = currentFrame;
+
+        // Handle user input
+        processInput(window);
+
+        // All the rendering starts from here
+
+        // We change to the custom framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glEnable(GL_DEPTH_TEST);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Set up view and projection matrix
+        glm::mat4 view = gCamera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(gCamera.Zoom),
+                                                (float)gScreenWidth / gScreenHeight, 0.1f, 100.0f);
+
+        // Draw the cubes
+        objectShader.use();
+        objectShader.setMat4("view", view);
+        objectShader.setMat4("projection", projection);
+        objectShader.setVec3("viewPos", gCamera.Position);
+
+        ambientMap.useTextureUnit(0);
+        specularMap.useTextureUnit(1);
+
+        // Draw cubes
+        glm::vec3 cubePositions[5] = {
+                glm::vec3(0.0f,  0.5f, 0.0f),
+                glm::vec3(2.0f, 0.5f, 2.0f),
+                glm::vec3(2.0f, 0.5f, -2.0f),
+                glm::vec3(-2.0f, 0.5f, -2.0f),
+                glm::vec3(-2.0f, 0.5, 2.0f),
+        };
+        glBindVertexArray(cubeVAO);
+        for (int i = 0; i < 5; ++i) {
+            // Compute model transformations for each cube
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+
+            objectShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        // Start to draw planes
+        // Draw grasses
+        glm::vec3 grassPositions[4] = {
+                glm::vec3(2.0f, 0.5f, 0.0f),
+                glm::vec3(0.0f, 0.5f, 2.0f),
+                glm::vec3(-2.0f, 0.5f, 0.0f),
+                glm::vec3(0.0f, 0.5f, -2.0f),
+        };
+        glBindVertexArray(planeVAO);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                glm::mat4 model = glm::mat4(1.0f);
+
+                model = glm::translate(model, grassPositions[i]);
+                model = glm::rotate(model, glm::radians(45.0f) * j, glm::vec3(0.0f, 1.0f, 0.0f));
+                model = glm::rotate(model, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+                objectShader.setMat4("model", model);
+                grassTexture.useTextureUnit(0);
+                // We set current texture to GL_CLAMP_TO_EDGE to prevent artifacts around the edge
+                // from interpolating near texture borders
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+                glDrawArrays(GL_TRIANGLES, 0, 6);
+            }
+        }
+
+        // Draw the ground
+        glBindVertexArray(groundVAO);
+        glm::mat4 planeModel = glm::mat4(1.0f);
+        planeModel = glm::scale(planeModel, glm::vec3(10.0f));
+        objectShader.use();
+        objectShader.setMat4("model", planeModel);
+        groundTexture.useTextureUnit(0);
+        // Use 0 to set the active texture to default texture
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        // Draw the transparent window
+        // It must be drawn in the last so that all other objects can be blended with them
+        // Also, if multiple transparent windows are involved, we MUST sort them and draw them
+        // from farther to nearest to avoid depth testing issues.
+        glBindVertexArray(planeVAO);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(3.0f, 0.8f, 0.0f));
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        transparentWindowShader.use();
+        transparentWindowShader.setMat4("view", view);
+        transparentWindowShader.setMat4("projection", projection);
+        transparentWindowShader.setVec3("viewPos", gCamera.Position);
+        transparentWindowShader.setMat4("model", model);
+        transparentWindowTexture.useTextureUnit(0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDisable(GL_DEPTH_TEST);
+
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Rendering Ends here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+GLFWwindow *init()
+{
+    // Initialization of GLFW context
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a window object
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Lighting Scene", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD before calling OpenGL functions
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return nullptr;
+    }
+
+    // Tell OpenGL the size of rendering window
+    glViewport(0, 0, gScreenWidth, gScreenHeight);
+
+    // Set the windows resize callback function
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    // Set up mouse input
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouseCallback);
+    glfwSetScrollCallback(window, scrollCallback);
+
+    return window;
+}
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    gScreenWidth = width;
+    gScreenHeight = height;
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    // Exit
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        gCamera.ProcessKeyboard(FORWARD, gDeltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        gCamera.ProcessKeyboard(BACKWARD, gDeltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        gCamera.ProcessKeyboard(LEFT, gDeltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        gCamera.ProcessKeyboard(RIGHT, gDeltaTime);
+    }
+}
+
+void mouseCallback(GLFWwindow *window, double xpos, double ypos)
+{
+    // Variables needed to handle mouse input
+    static float lastMouseX = 400.0f;
+    static float lastMouseY = 300.0f;
+    static bool firstMouse = true;
+
+    if (firstMouse) {
+        lastMouseX = (float)xpos;
+        lastMouseY = (float)ypos;
+        firstMouse = false;
+    }
+
+    // Calculate mouse movement since last frame
+    float offsetX = (float)xpos - lastMouseX;
+    float offsetY = (float)ypos - lastMouseY;
+    lastMouseX = (float)xpos;
+    lastMouseY = (float)ypos;
+
+    gCamera.ProcessMouseMovement(offsetX, offsetY);
+}
+
+void scrollCallback(GLFWwindow *window, double offsetX, double offsetY)
+{
+    gCamera.ProcessMouseScroll((float)offsetY);
+}
